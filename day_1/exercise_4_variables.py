cf4bb183e031b98825e144eb9fdf6ad31c9b664c
José Pedro <jpedroramos96@gmail.com>
2022-12-12 16:09:51 +0000

    New files added

---
+++ day_1/exercise_4_variables.py
@@ -0,0 +1,10 @@
+a = input("a: ")
+b = input("b: ")
+
+aux1 = a
+aux2 = b
+a = aux2
+b = aux1
+
+print("a: " + a)
+print("b: " + b)
---
+++ day_1/variable_naming.py
@@ -0,0 +1,2 @@
+user_name = input("What's your name?\n")
+print(user_name)
