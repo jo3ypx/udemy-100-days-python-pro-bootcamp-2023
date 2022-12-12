2e49a67c6a1a448391656fdf87698b704587e8ee
José Pedro <jpedroramos96@gmail.com>
2022-12-12 23:57:26 +0000

    Added 'number_manipulation_f_strings.py'

---
+++ day_2/number_manipulation_f_strings.py
@@ -0,0 +1,22 @@
+print(round(8 / 3))
+print(round(8 / 3, 2))
+print(round(2.6666666, 2))
+print(8 // 3)
+print(type(8 // 3))
+
+result = 4 / 2
+result /= 2
+print(result)
+
+score = 0
+score += 1
+print(score)
+score -= 1
+print(score)
+
+score = 0
+height = 1.8
+isWinning = True
+
+print(
+    f"Your score is {score}, your height is {height}, you are winning is {isWinning}.")
