771825b198ea1f8e51cb86b8497823567b65aaa6
José Pedro <jpedroramos96@gmail.com>
2022-12-12 16:48:21 +0000

    Modified "project_band_name_generator.py"

--- day_1/project_band_name_generator.py
+++ day_1/project_band_name_generator.py
@@ -1,6 +1,15 @@
+# Version 1
+
+# print("Welcome to the Band Name Generator.")
+# print("What's the name of the city you grew up in?\n")
+# city_name = input()
+# print("What's your pet's name?\n")
+# pet_name = input()
+# print("Your band name could be " + city_name + " " + pet_name)
+
+# Version 2
+
 print("Welcome to the Band Name Generator.")
-print("What's the name of the city you grew up in?")
-city_name = input()
-print("What's your pet's name?")
-pet_name = input()
+city_name = input("What's the name of the city you grew up in?\n")
+pet_name = input("What's your pet's name?\n")
 print("Your band name could be " + city_name + " " + pet_name)
