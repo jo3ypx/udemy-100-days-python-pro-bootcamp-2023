344f18ff26e990600aa741cbc00de824a2d9c83b
José Pedro <jpedroramos96@gmail.com>
2022-12-12 19:02:56 +0000

    Edited "type_error_checking_conversion.py" and added "exercise_1_data_types.py"

---
+++ day_2/exercise_1_data_types.py
@@ -0,0 +1,6 @@
+two_digit_number = input("Type a two digit number: ")
+
+number_1 = int(two_digit_number[0])
+number_2 = int(two_digit_number[1])
+
+print(number_1 + number_2)
--- day_2/type_error_checking_conversion.py
+++ day_2/type_error_checking_conversion.py
@@ -2,6 +2,21 @@
 # len(4837)
 
 # len() returns an integer
-num_char = len(input("What's your name?\n"))
+# num_char = len(input("What's your name?\n"))
 # can only concatenate strings
-print("Your name has " + num_char + " characters.")
+# print("Your name has " + num_char + " characters.")
+# print(type(num_char))
+
+# num_char = len(input("What's your name?\n"))
+# num_char_converted = str(num_char)  # convert integer to string
+# print("Your name has " + num_char_converted + " characters.")
+# print(type(num_char_converted))
+
+a = 123
+print(type(a))
+
+b = float(123)
+print(type(b))
+
+print(70 + float("100.5"))
+print(str[70] + str(100))
