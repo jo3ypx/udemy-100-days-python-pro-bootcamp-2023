c3e0558a815118b52d25f06027b9fee8434a0e47
José Pedro <jpedroramos96@gmail.com>
2022-12-14 19:47:34 +0000

    Added 'nested_statements.py'

---
+++ day_3/nested_statements.py
@@ -0,0 +1,26 @@
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm?\n"))
+
+# Nested if statements
+# if height >= 120:
+#    print("You can ride the rollercoaster!")
+#    age = int(input("What's your age?\n"))
+#    if age <= 18:
+#        print("Please pay $7.")
+#    else:
+#        print("Please pay $12.")
+# else:
+#    print("Sorry, you have to grow taller before you can ride.")
+
+# elif statements
+if height >= 120:
+    print("You can ride the rollercoaster!")
+    age = int(input("What's your age?\n"))
+    if age < 12:
+        print("Please pay $5.")
+    elif age <= 18:
+        print("Please pay $7.")
+    else:
+        print("Please pay $12.")
+else:
+    print("Sorry, you have to grow taller before you can ride.")
