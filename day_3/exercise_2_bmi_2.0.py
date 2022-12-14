757e24ed358241f249a719dac20705259431e9fb
José Pedro <jpedroramos96@gmail.com>
2022-12-14 23:20:27 +0000

    Modified 'exercise_2_bmi_2.py'

--- day_3/exercise_2_bmi_2.0.py
+++ day_3/exercise_2_bmi_2.0.py
@@ -1,2 +1,16 @@
-height = float(input("Enter your height in m:\n"))
-weight = float(input("Enter your weight in kg:\n"))
+import math
+
+height = input("Enter your height in m:\n")
+weight = input("Enter your weight in kg:\n")
+bmi = round(int(weight) / math.pow(float(height), 2))
+
+if bmi < 18.5:
+    print("Your BMI is " + str(bmi) + ", you are underweight.")
+elif bmi <= 25:
+    print("Your BMI is " + str(bmi) + ", you have a normal weight.")
+elif bmi <= 30:
+    print("Your BMI is " + str(bmi) + ", you are slightly overweight.")
+elif bmi <= 35:
+    print("Your BMI is " + str(bmi) + ", you are obese.")
+elif bmi > 35:
+    print("Your BMI is " + str(bmi) + ", you are clinically obese.")
