a727069389716b973f21a5b27804d3205ca5bfaa
José Pedro <jpedroramos96@gmail.com>
2022-12-22 00:38:43 +0000

    Added 'exercise_4_fizzbuzz.py'

---
+++ day_5/exercise_4_fizzbuzz.py
@@ -0,0 +1,10 @@
+for number in range(1, 101):
+    # verify if the number is divisible by both 3 and 5
+    if number % 3 == 0 and number % 5 == 0:
+        print("FizzBuzz")
+    elif number % 3 == 0:
+        print("Fizz")
+    elif number % 5 == 0:
+        print("Buzz")
+    else:
+        print(number)
