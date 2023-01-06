8679e0a2d5706cd7f8519c6f895e210e1ca158b6
José Pedro <jpedroramos96@gmail.com>
2023-01-06 19:25:13 +0000

    Modified 'ha.py'

--- day_7/hangman.py
+++ day_7/hangman.py
@@ -1,20 +1,18 @@
 import random as r
 
 word_list = ["aardvark", "baboon", "camel"]
-# chosen_word = word_list[r.randint(0, len(word_list)-1)]
 blank = ""
 chosen_word = r.choice(word_list)
 for n in range(len(chosen_word)):
     blank += "_"
 
-    # while:
+# while:
 guess = input("Guess a letter:\n").lower()
 for letter in chosen_word:
     if guess != letter:
-        print()
+        print(blank)
         print("You guessed " + guess +
               ", that's not in the word. You lose a life.")
-        print()
 
         # TODO-3 - Check if the letter the user guessed (guess)
         # is one of the letters in the chosen_word.
