b8abb5cdb2414ca627750eaf23beb01b2747c986
José Pedro <jpedroramos96@gmail.com>
2023-01-07 22:49:53 +0000

    Added 'functions_input.py'

---
+++ day_8/functions_input.py
@@ -0,0 +1,19 @@
+# Simple function
+def greet():
+    print("Hello.")
+    print("How do you do?")
+    print("Isn't the weather nice today?")
+
+
+greet()
+
+# Function with input
+
+
+def greet_with_name(name):
+    print("Hello " + name + ".")
+    print("How do you do, " + name + "?")
+    print("Isn't the weather nice today?")
+
+
+greet_with_name("José")
