91b913042ebfeb7d73eaa9c71047cbe99372ca9f
José Pedro <jpedroramos96@gmail.com>
2023-01-07 22:18:25 +0000

    Added ''

--- day_7/hangman.py
+++ day_7/hangman.py
@@ -1,18 +1,21 @@
 import random as r
+import hangman_art as art
+import hangman_words as words
 
-word_list = ["aardvark", "baboon", "camel"]
 blank = ""
-chosen_word = r.choice(word_list)
+lives = 5
+chosen_word = r.choice(words.word_list)
 for n in range(len(chosen_word)):
     blank += " _"
 
-# while:
-guess = input("Guess a letter:\n").lower()
-for letter in chosen_word:
-    if guess != letter:
-        print(blank)
-        print("You guessed " + guess +
-              ", that's not in the word. You lose a life.")
+print(art.logo)
+while lives != 0:
+    guess = input("Guess a letter:\n").lower()
+    for letter in chosen_word:
+        if guess != letter:
+            print(blank)
+            print("You guessed " + guess +
+                  ", that's not in the word. You lose a life.")
 
         # TODO-3 - Check if the letter the user guessed (guess)
         # is one of the letters in the chosen_word.
